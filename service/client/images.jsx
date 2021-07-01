d1dc4a56837694dca2a091c1125416ebe0c2081e
houssembouraioui <houssembouraoui6@gmail.com>
2021-07-01 20:57:41 +0100

    i'm gonna fuck it up for sure

--- service/client/images.jsx
+++ service/client/images.jsx
@@ -1,6 +1,7 @@
 import "./index.css";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+// import { arrowLeft } from "@heroicons/react";
 
 const Images = () => {
   const [images, setImages] = useState([]);
@@ -26,16 +27,58 @@ const Images = () => {
   }, []);
   return (
     <div className=" flex gap-4">
-      <ul className="">
-        {images.map((product) => {
-          return (
-            <li className="h-18 w-10" key={product.id}>
-              <img src={product.thumbnail} />
-            </li>
-          );
-        })}
-      </ul>
+      <div className="space-y-4 space-x-6">
+        <ul className="space-y-4 space-x-4">
+          {images.map((product) => {
+            return (
+              <li className="h-18 w-10 " key={product.id}>
+                <img src={product.thumbnail} />
+              </li>
+            );
+          })}
+        </ul>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="grey"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M19 9l-7 7-7-7"
+          />
+        </svg>
+      </div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6 space-y-40"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="grey"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M10 19l-7-7m0 0l7-7m-7 7h18"
+        />
+      </svg>
       {images.length && <img className="" src={images[4].main} />}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        viewBox="0 0 20 20"
+        fill="grey"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+          clipRule="evenodd"
+        />
+      </svg>
     </div>
   );
 };
